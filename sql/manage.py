bbe7f1df31e4a46c15280c628cba75488715982a
GabrielTorland <73033765+GabrielTorland@users.noreply.github.com>
2024-03-11 12:59:57 +0100

    Process csv in chunks for memory efficiency

--- sql/manage.py
+++ sql/manage.py
@@ -4,33 +4,34 @@ from sqlalchemy.orm import sessionmaker
 from models import *
 import pandas as pd
 
-def insert_from_csv(csv_file_path, session):
-    df = pd.read_csv(csv_file_path, nrows=10000)
-    
-    for index, row in df.iterrows():
-        if row.isnull().any():
-            print(f"Skipping row {index} due to NaN values: {[col for col, value in row.items() if pd.isnull(value)]}")
-            continue
-        
-        user = User(user_id=row['user_id'])
-        session.merge(user)
+def insert_from_csv(csv_file_path, session, chunksize=1000):
+    # Iterate over the CSV file in chunks
+    for chunk in pd.read_csv(csv_file_path, chunksize=chunksize):
+        for index, row in chunk.iterrows():
+            if row.isnull().any():
+                print(f"Skipping row {index} due to NaN values: {[col for col, value in row.items() if pd.isnull(value)]}")
+                continue
+            
+            user = User(user_id=row['user_id'])
+            session.merge(user)
 
-        category = Category(category_id=row['category_id'], category_code=row['category_code'])  # Adjust as necessary
-        session.merge(category)
+            category = Category(category_id=row['category_id'], category_code=row['category_code'])  # Adjust as necessary
+            session.merge(category)
 
-        product = Product(product_id=row['product_id'], category_id=row['category_id'], brand=row['brand'], price=row['price'])
-        session.merge(product)
+            product = Product(product_id=row['product_id'], category_id=row['category_id'], brand=row['brand'], price=row['price'])
+            session.merge(product)
 
-        event_date = pd.to_datetime(row['event_time'], errors='coerce', format='%Y-%m-%d %H:%M:%S %Z')
-        event = Event(event_time=event_date, event_type=row['event_type'], product_id=row['product_id'], user_id=row['user_id'], user_session=row['user_session'])
-        session.merge(event)
+            event_date = pd.to_datetime(row['event_time'], errors='coerce', format='%Y-%m-%d %H:%M:%S %Z')
+            event = Event(event_time=event_date, event_type=row['event_type'], product_id=row['product_id'], user_id=row['user_id'], user_session=row['user_session'])
+            session.merge(event)
 
-        print(f"Inserted row {index} into database.")
+            print(f"Inserted row {index} into database.")
 
-        if index % 100 == 0:  # Commit per 100 rows inserted to avoid locking the table for too long
-            session.commit()
-    
-    session.commit()  # Final commit to save any remaining data
+        print("Committing the current chunk.")
+        session.commit()  # Commit after each chunk is processed
+
+    print("All data committed.")
+
 
 
 # Setup argparse for script options
