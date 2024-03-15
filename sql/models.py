4861d6e17dc0382ac838f53c4722e48605a0a5c6
zedyz <fredrikolsen94@gmail.com>
2024-03-15 14:00:42 +0100

    updated varchar values to reflect biggest values

--- sql/models.py
+++ sql/models.py
@@ -7,18 +7,18 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'Users'
-    user_id = Column(String(255), primary_key=True)
+    user_id = Column(String(9), primary_key=True)
 
 class Category(Base):
     __tablename__ = 'Categories'
-    category_id = Column(String(255), primary_key=True)
-    category_code = Column(String(255), default=None)
+    category_id = Column(String(19), primary_key=True)
+    category_code = Column(String(38), default=None)
 
 class Product(Base):
     __tablename__ = 'Products'
-    product_id = Column(String(255), primary_key=True)
-    category_id = Column(String(255), ForeignKey('Categories.category_id'))
-    brand = Column(String(255), default=None)
+    product_id = Column(String(9), primary_key=True)
+    category_id = Column(String(19), ForeignKey('Categories.category_id'))
+    brand = Column(String(28), default=None)
     price = Column(DECIMAL(10, 2))
     category = relationship("Category")
 
@@ -26,9 +26,9 @@ class Event(Base):
     __tablename__ = 'Events'
     event_id = Column(BigInteger, primary_key=True, autoincrement=True)
     event_time = Column(TIMESTAMP)
-    event_type = Column(String(50))
-    product_id = Column(String(255), ForeignKey('Products.product_id'))
-    user_id = Column(String(255), ForeignKey('Users.user_id'))
-    user_session = Column(String(255))
+    event_type = Column(String(8), default=None)
+    product_id = Column(String(9), ForeignKey('Products.product_id'))
+    user_id = Column(String(9), ForeignKey('Users.user_id'))
+    user_session = Column(String(36), default=None)
     product = relationship("Product")
     user = relationship("User")
